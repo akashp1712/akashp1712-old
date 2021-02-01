5f6dd1d8de5b092e8acc27fdeccd61e7a5ac59a4
akashp1712 <akashp1712@gmail.com>
2021-02-01 15:07:04 +0000

    Deploying to gh-pages from  @ cc5946d90cd54ebcd39f31ab471a7b761f01827a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb54fceb95226be66ef2b457ac016a04.js"
+  "/developerFolio/precache-manifest.e98e124ac9fb4ab761eb9ef71437b766.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
