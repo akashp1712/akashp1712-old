8634194bf9c841aa5fe66e75972a53d1fd3f1e6d
Akash Panchal <akashp1712@gmail.com>
2021-06-01 10:36:06 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,7 +12,7 @@ const greeting = {
   username: "Akash Panchal",
   title: "Hello World, I'm Akash Panchal",
   subTitle: emoji(
-    "A passionate Software Engineer with experience of building Web and Mobile applications with Java / Python / JavaScript and some other cool projects for Machine Learning and NLP."
+    "A passionate Software Engineer with experience of building Microservices based applications with Python, JavaScript and some other cool projects for Mobile, Machine Learning and NLP."
   ),
   resumeLink:
     "https://github.com/akashp1712/developerFolio/blob/master/Akash-Panchal-Resume.pdf",
